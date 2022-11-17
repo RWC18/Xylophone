af732068674f3616d075867c05cdf75f0d2025c8
Vahe Minasyan <minasyan.vahe@picsart.com>
2022-11-17 15:11:32 +0400

    Finish v1

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(btn: (sender.currentTitle)!)
+        sender.alpha = 0.7
+                
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    sender.alpha = 1.0
+            
+         }
+       }
+       
+    func playSound(btn: String) {
+           let url = Bundle.main.url(forResource: btn, withExtension: "wav")
+           player = try! AVAudioPlayer(contentsOf: url!)
+           player?.play()
+                   
+       }
+
 
 }
 
